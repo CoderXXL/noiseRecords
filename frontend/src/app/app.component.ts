a7b184b91d531f7d6a971014a11eead3e907b1d5
Dario Lang <dario.lang@flagbit.de>
2019-05-07 11:50:05 +0200

    integrating Flask and Angular

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ExamsApiService } from './exams/exams-api.service';
+import { Exam } from './exams/exam.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit, OnDestroy {
+  title = 'noiseRecords';
+  examsListSubs: Subscription;
+  examsList: Exam[];
+  constructor(private examsApi: ExamsApiService) {}
+
+  ngOnInit() {
+    this.examsListSubs = this.examsApi.getExams().subscribe(res => {
+      this.examsList = res;
+      console.log(res);
+    }, console.error);
+  }
+
+  ngOnDestroy() {
+    this.examsListSubs.unsubscribe();
+  }
+}
